f53d91cc2dc0afd9c06bdd8df9653ad05c441485
Jorge <jorgelcs85@gmail.com>
2021-05-22 01:41:24 -0400

    expande container alt

--- src/componets/templates/Container.tsx
+++ src/componets/templates/Container.tsx
@@ -13,17 +13,19 @@ interface IProp {
 
 const Container = ({ title, sections, image, position = 'default', alt }: IProp) => {
   return (
-    <section className={`container container--${position} ${alt ? 'container--alt' : ''}`}>
-      {title && (<h2 className="container__title">{title}</h2>)}
-      <div className={`container__info container__info--${position}`}>
-        {sections.map(section =>
-          <Section title={section.title} alt={alt}>
-            {section.description}
-          </Section>
-        )}
-      </div>
-      <div className={`container__image container__image--${position} ${alt ? 'container__image--alt' : ''}`}>
-        <img src={image} alt="section" />
+    <section className={`container ${alt ? 'container--alt' : ''}`}>
+      <div className={`container__content container__content--${position}`}>
+        {title && (<h2 className="container__title">{title}</h2>)}
+        <div className={`container__info container__info--${position}`}>
+          {sections.map(section =>
+            <Section title={section.title} alt={alt}>
+              {section.description}
+            </Section>
+          )}
+        </div>
+        <div className={`container__image container__image--${position} ${alt ? 'container__image--alt' : ''}`}>
+          <img src={image} alt="section" />
+        </div>
       </div>
     </section>
   )
